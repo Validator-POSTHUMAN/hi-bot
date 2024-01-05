369fb5b323eb3d21c6b25c7d1d328a569c3c2c73
stribulsergey <ssargiuz@gmail.com>
2024-01-04 22:12:45 -0300

    add notification for empty groups list

--- src/modules/on-get-message.ts
+++ src/modules/on-get-message.ts
@@ -3,19 +3,23 @@ import { IBotContext } from '../bot.types.js';
 import groupService from './group/group.service.js';
 
 export const onGetMessage = (pm: Composer<ChatTypeContext<IBotContext, 'private'>>) => {
-  pm.on('message', async (ctx) => {
+  pm.on('message', async (ctx): Promise<void> => {
     const groups = await groupService.findAll();
 
-    await ctx.reply('Where you want to send the message?', {
-      parse_mode: 'HTML',
-      reply_markup: {
-        inline_keyboard: groups.map((group) => [
-          {
-            text: group.name,
-            callback_data: `cb-copy-to-group?groupId=${group.chatId}&messageId=${ctx.message.message_id}`,
-          },
-        ]),
-      },
-    });
+    if (groups.length > 0) {
+      await ctx.reply('Where you want to send the message?', {
+        parse_mode: 'HTML',
+        reply_markup: {
+          inline_keyboard: groups.map((group) => [
+            {
+              text: group.name,
+              callback_data: `cb-copy-to-group?groupId=${group.chatId}&messageId=${ctx.message.message_id}`,
+            },
+          ]),
+        },
+      });
+    } else {
+      await ctx.reply('Firstly you need to add bot to groups and channels', { parse_mode: 'MarkdownV2' });
+    }
   });
 };
